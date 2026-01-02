52c46c6078cf541edf27e2502a7561778f7136dc
Pranav Deshmukh <151390018+prn9v@users.noreply.github.com>
2026-01-02 20:28:00 +0530

    Time: 347 ms (15.59%) | Memory: 47 MB (41.24%) - LeetSync

---
+++ 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] findEvenNumbers(int[] digits) {
+        boolean[] used = new boolean[digits.length];
+        HashSet<Integer> set = new HashSet<>();
+        int sum = 0;
+        int depth = 0;
+        helper(set,used,sum,depth,digits);
+        int[] ans = set.stream().mapToInt(Integer::intValue).toArray();
+        Arrays.sort(ans);
+        return ans;
+    }
+
+    private void helper(HashSet<Integer> set,boolean[] used,int sum,int depth,int[] digits){
+        if (depth == 3) {
+            if (sum % 2 == 0) {
+                set.add(sum);
+            }
+            return;
+        }
+
+        for (int i = 0; i < digits.length; i++) {
+            if (used[i]) continue;
+            if (depth == 0 && digits[i] == 0) continue;
+
+            used[i] = true;
+            helper(set, used, sum * 10 + digits[i], depth+1, digits);
+            used[i] = false;
+        }
+    }
+}
