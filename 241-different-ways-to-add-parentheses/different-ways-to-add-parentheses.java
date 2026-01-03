bd7b2ec0b492c11840b0d9f639916be0c06ebb04
Pranav Deshmukh <151390018+prn9v@users.noreply.github.com>
2026-01-03 19:20:30 +0530

    Time: 3 ms (51.17%) | Memory: 44.4 MB (36.97%) - LeetSync

--- 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.java
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.java
@@ -1,39 +1,31 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
     public List<Integer> diffWaysToCompute(String expression) {
-        List<Integer> list = new ArrayList<>();
-        List<Integer> leftResult;
-        List<Integer> rightResult;
+
+        List<Integer> result = new ArrayList<>();
 
         for (int i = 0; i < expression.length(); i++) {
-            char ch = expression.charAt(i);
 
-            if (ch == '+' || ch == '-' || ch == '*') {
-                leftResult = diffWaysToCompute(expression.substring(0, i));
-                rightResult = diffWaysToCompute(expression.substring(i + 1));
+            char c = expression.charAt(i);
 
-                for (int j = 0; j < leftResult.size(); j++) {
-                    int left = leftResult.get(j);
-                    for (int k = 0; k < rightResult.size(); k++) {
-                        int right = rightResult.get(k);
-                        if (ch == '+') {
-                            list.add(left + right);
-                        } else if (ch == '-') {
-                            list.add(left - right);
-                        } else {
-                            list.add(left * right);
-                        }
+            if (c == '+' || c == '-' || c == '*') {
+
+                List<Integer> left = diffWaysToCompute(expression.substring(0, i));
+                List<Integer> right = diffWaysToCompute(expression.substring(i + 1));
+
+                for (int a : left) {
+                    for (int b : right) {
+                        if (c == '+') result.add(a + b);
+                        else if (c == '-') result.add(a - b);
+                        else result.add(a * b);
                     }
                 }
             }
         }
 
-        if (list.isEmpty()) {
-            list.add(Integer.parseInt(expression));
+        if (result.isEmpty()) {
+            result.add(Integer.parseInt(expression));
         }
 
-        return list;
+        return result;
     }
 }
