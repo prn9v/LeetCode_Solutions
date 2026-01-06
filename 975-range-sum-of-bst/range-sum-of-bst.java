571f54330f38951f56a2a7be383c6ced29e5412c
Pranav Deshmukh <151390018+prn9v@users.noreply.github.com>
2026-01-06 22:36:50 +0530

    Time: 0 ms (100.00%) | Memory: 54.1 MB (25.12%) - LeetSync

--- 975-range-sum-of-bst/range-sum-of-bst.java
+++ 975-range-sum-of-bst/range-sum-of-bst.java
@@ -1,33 +1,28 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        return helper(root, low, high);
+    }
 
-      public int rangeSumBST(TreeNode root, int low, int high) {
-
-        if(root == null){
+    private int helper(TreeNode root, int low, int high) {
+        if (root == null) {
             return 0;
         }
 
-        if(root.val>=low && root.val<=high){
-            return rangeSumBST(root.left,low,high)+root.val+rangeSumBST(root.right,low,high);
-        }else if(root.val<=low){
-            return rangeSumBST(root.right,low,high);
-        }else{
-            return rangeSumBST(root.left,low,high);
-        }
-    }
+        int sum = 0;
 
+        // BST pruning
+        if (root.val > low) {
+            sum += helper(root.left, low, high);
+        }
+
+        if (root.val >= low && root.val <= high) {
+            sum += root.val;
+        }
+
+        if (root.val < high) {
+            sum += helper(root.right, low, high);
+        }
+
+        return sum;
+    }
 }
