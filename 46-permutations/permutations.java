f5ff2067a3b26cda75937ec3c656edd9a9c71288
Pranav Deshmukh <151390018+prn9v@users.noreply.github.com>
2026-01-24 18:40:04 +0530

    Time: 1 ms (87.20%) | Memory: 45.6 MB (38.76%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,29 +1,29 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> list = new ArrayList<>();
-        helper(0,nums,list,ans);
+        boolean[] used = new boolean[nums.length];
+        helper(nums,used,new ArrayList<>(),ans);
         return ans;
     }
 
-    private void helper(int index,int[] nums,List<Integer> list,List<List<Integer>> ans){
-        if (index == nums.length){
+    private void helper(int[] nums,boolean[] used,List<Integer> list,List<List<Integer>> ans) {
+        if(list.size() == nums.length) {
             ans.add(new ArrayList<>(list));
             return;
         }
 
-        for(int i = 0; i < nums.length; i++){
-            if (!list.contains(nums[i])) {
-                list.add(nums[i]);
-                helper(index + 1, nums, list, ans);
-                list.remove(list.size() - 1); 
+        for(int i = 0; i < nums.length; i++) {
+            if(used[i]){
+                continue;
             }
-        }
-    }
 
-    private void swap(int i,int j,int[] nums){
-        int t = nums[i];
-        nums[i] = nums[j];
-        nums[j] = t;
+            used[i] = true;
+            list.add(nums[i]);
+
+            helper(nums,used,list,ans);
+
+            list.remove(list.size() - 1);
+            used[i] = false;
+        }
     }
 }
