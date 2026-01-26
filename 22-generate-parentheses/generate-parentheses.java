e88fdab46fe5bfd35dc306495f7ad855d420317e
Pranav Deshmukh <151390018+prn9v@users.noreply.github.com>
2026-01-26 21:06:56 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (89.29%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,24 +1,26 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-         ArrayList<String> ans = new ArrayList<>();
-         StringBuilder str = new StringBuilder();
-         helper(n,0,0,str,ans);
-         return ans;
-    }        
+        List<String> list = new ArrayList<>();
+        StringBuilder str = new StringBuilder();
+        helper(list, 0, 0, str, n);
+        return list;
+    }
 
-    private void helper(int n, int left, int right, StringBuilder str, ArrayList<String> ans){
-        if(left + right == (2*n)){
-            ans.add(str.toString());
+    private void helper(List<String> list, int left, int right, StringBuilder str, int n) {
+        if (left == n && right == n) {
+            list.add(str.toString());
             return;
         }
 
-        if(left < n){
-            helper(n,left+1,right,str.append("("),ans);
+        if (left < n) {
+            str.append('(');
+            helper(list, left + 1, right, str, n);
             str.deleteCharAt(str.length() - 1);
-        } 
+        }
 
-        if(right < left) {
-            helper(n,left,right+1,str.append(")"),ans);
+        if (right < left) {
+            str.append(')');
+            helper(list, left, right + 1, str, n);
             str.deleteCharAt(str.length() - 1);
         }
     }
