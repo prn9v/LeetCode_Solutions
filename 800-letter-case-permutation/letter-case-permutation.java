26c097516280c3473587e0364e126042a3cc7db5
Pranav Deshmukh <151390018+prn9v@users.noreply.github.com>
2026-02-13 19:37:20 +0530

    Time: 1 ms (99.78%) | Memory: 47.4 MB (71.03%) - LeetSync

---
+++ 800-letter-case-permutation/letter-case-permutation.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<String> letterCasePermutation(String s) {
+        List<String> ans = new ArrayList<>();
+        StringBuilder str = new StringBuilder(s);
+        helper(str, ans, 0);
+        return ans;
+    }
+
+    private void helper(StringBuilder sb, List<String> ans, int index) {
+        if (index == sb.length()) {
+            ans.add(sb.toString());
+            return;
+        }
+
+        char original = sb.charAt(index);
+
+        if (Character.isDigit(sb.charAt(index))) {
+            helper(sb, ans, index+1);
+        } else {
+            sb.setCharAt(index, Character.toLowerCase(original));
+            helper(sb, ans, index + 1);
+
+            sb.setCharAt(index, Character.toUpperCase(original));
+            helper(sb, ans, index + 1);
+
+            sb.setCharAt(index, original);
+        }
+    }
+}
