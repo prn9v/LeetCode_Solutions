7c02d47eb3024b6309cf03a8c3e01c242c55255d
Pranav Deshmukh <151390018+prn9v@users.noreply.github.com>
2026-03-03 18:44:23 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (87.38%) - LeetSync

--- 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.java
+++ 1667-find-kth-bit-in-nth-binary-string/find-kth-bit-in-nth-binary-string.java
@@ -1,25 +1,18 @@
 public class Solution {
 
     public char findKthBit(int n, int k) {
-        StringBuilder str = new StringBuilder("0");
-        return helper(str,n,k,1);
-    }
+        if (n == 1) return '0';
 
-    private char helper(StringBuilder str,int n, int k,int index){
-        if(index == n){
-            return str.charAt(k-1);
+        int len = (1 << n) - 1;
+        int mid = (len + 1) / 2;
+
+        if (k == mid) {
+            return '1';
+        } else if (k < mid) {
+            return findKthBit(n - 1, k);
+        } else {
+            char bit = findKthBit(n - 1, len - k + 1);
+            return bit == '0' ? '1' : '0';
         }
-
-        int len = str.length();
-        StringBuilder newStr = new StringBuilder();
-        for(int i = 0; i < len; i++){
-            newStr.append(str.charAt(i) == '0' ? '1' : '0');
-        }
-
-        newStr.reverse();
-        str.append('1');           
-        str.append(newStr);
-
-        return helper(str,n,k,index+1);
     }
 }
