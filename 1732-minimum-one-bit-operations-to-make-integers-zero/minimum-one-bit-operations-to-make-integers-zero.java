4b05842bf74dcaeabbe5da2a71e1b61700ed6161
Pranav Deshmukh <151390018+prn9v@users.noreply.github.com>
2026-03-05 22:17:58 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (61.22%) - LeetSync

--- 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.java
+++ 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.java
@@ -1,28 +1,10 @@
 class Solution {
     public int minimumOneBitOperations(int n) {
-        Stack<Integer> stack = new Stack<>();
-        int num = n;
-        
-        int i = 1;
-        while(num != 0){
-            if((num & 1) == 1){
-                stack.push((int)(Math.pow(2,i)-1));
-            }
-            i++;
-            num = num >> 1;
+        int res =0;
+        while(n > 0){
+            res  = res ^ n;
+            n = n>>1;
         }
-
-        int ans = 0;
-        boolean add = true;
-        while(!stack.isEmpty()){
-            if(add){
-                ans += stack.pop();
-            } else {
-                ans -= stack.pop();
-            }
-            add = !add;
-        }
-
-        return ans;
+        return res;
     }
 }
