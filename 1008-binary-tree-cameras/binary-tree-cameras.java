bdb5c02b1a888fda475899811d8984695f16fd30
Pranav Deshmukh <151390018+prn9v@users.noreply.github.com>
2026-03-08 15:28:29 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (73.48%) - LeetSync

--- 1008-binary-tree-cameras/binary-tree-cameras.java
+++ 1008-binary-tree-cameras/binary-tree-cameras.java
@@ -1,86 +1,29 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
+    private int count;
 
-    class Node {
-        TreeNode root;
-        char ch;
-
-        Node(TreeNode root, char ch) {
-            this.root = root;
-            this.ch = ch;
-        }
-    }
-
-    public int minCameraCover(TreeNode root) {
+    private int dfs(TreeNode root) {
         if (root == null) {
             return 0;
         }
 
-        if (root.left == null && root.right == null) {
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+
+        if (Math.min(left, right) == -1) {
+            this.count++;
             return 1;
         }
         
-        Node stateTree = createStateTree(root);
-        int cameras =  countCameras(stateTree);
-
-        if(stateTree.ch == 'R'){
-            cameras += 1;
-        }
-
-        return cameras;
-    }
-
-    private int countCameras(Node node) {
-        if (node == null || node.root == null) {
+        if (Math.max(left, right) == 1) {
             return 0;
         }
-
-        int count = 0;
-
-        if (node.ch == 'C') {
-            count++;
-        }
-
-        count += countCameras(createStateTree(node.root.left));
-        count += countCameras(createStateTree(node.root.right));
-
-        return count;
+        
+        return -1;
     }
 
-    private Node createStateTree(TreeNode root) {
-        if (root == null) {
-            return new Node(root, 'N');
-        }
-
-        Node leftState = createStateTree(root.left);
-        Node rightState = createStateTree(root.right);
-
-        Node node;
-
-        if (leftState.ch == 'R' || rightState.ch == 'R') {
-            node = new Node(root, 'C');
-        } 
-        else if (leftState.ch == 'C' || rightState.ch == 'C') {
-            node = new Node(root, 'N');
-        } 
-        else {
-            node = new Node(root, 'R');
-        }
-
-        return node;
+    public int minCameraCover(TreeNode root) {
+        this.count = 0;
+        int x = dfs(root);
+        return (x == -1) ? this.count + 1 : this.count;
     }
 }
