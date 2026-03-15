ff73a18871be73768bf9232d3f25164d30108eeb
Pranav Deshmukh <151390018+prn9v@users.noreply.github.com>
2026-03-15 15:15:13 +0530

    Time: 19 ms (78.35%) | Memory: 101.8 MB (73.16%) - LeetSync

--- 2597-cycle-length-queries-in-a-tree/cycle-length-queries-in-a-tree.java
+++ 2597-cycle-length-queries-in-a-tree/cycle-length-queries-in-a-tree.java
@@ -1,35 +1,17 @@
 class Solution {
     public int[] cycleLengthQueries(int n, int[][] queries) {
-        int m = queries.length;
-        int[] ans = new int[m];
-        
-        for (int i = 0; i < queries.length; i++) {
-            int a = queries[i][0];
-            int b = queries[i][1];
-
-            ans[i] = depth(a) + depth(b) - (2 * depth(lca(a,b))) + 1;
-        }
-
-        return ans;
-    }
-
-    private int depth(int a){
-        int d = 1;
-        while (a != 1) {
-            a=a/2;
-            d++;
-        }
-        return d;
-    }
-
-    private int lca(int a,int b){
-        while (a != b) {
-            if (a > b) {
-                a = a/2;
-            } else {
-                b = b/2;
+        int m = queries.length, res[] = new int[m];
+        for (int i = 0; i < m; ++i) {
+            res[i]++;
+            int x = queries[i][0], y = queries[i][1];
+            while (x != y) {
+                if (x > y)
+                    x /= 2;
+                else
+                    y /= 2;
+                res[i]++;
             }
         }
-        return a;
+        return res;
     }
 }
