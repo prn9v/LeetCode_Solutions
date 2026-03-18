93b4840c6b9c6aa20a47b7483aaa0fc6f7f9cb60
Pranav Deshmukh <151390018+prn9v@users.noreply.github.com>
2026-03-18 23:28:39 +0530

    Time: 2 ms (44.65%) | Memory: 43 MB (54.42%) - LeetSync

--- 60-permutation-sequence/permutation-sequence.java
+++ 60-permutation-sequence/permutation-sequence.java
@@ -1,28 +1,25 @@
 class Solution {
     public String getPermutation(int n, int k) {
-        List<String> list = new ArrayList<>();
-        StringBuilder str = new StringBuilder();
-        boolean[] visited = new boolean[n+1];
-        helper(list,str,0,visited,n);
-        return list.get(k-1);
-    }
+        List<Integer> numbers = new ArrayList<>();
+        int[] fact = new int[n + 1];
 
-    private void helper(List<String> list,StringBuilder str,int index,boolean[] visited,int n){
-        if (index == n) {
-            list.add(str.toString());
-            return;
-        }
-
-        int len = str.length();
+        fact[0] = 1;
         for (int i = 1; i <= n; i++) {
-            if (visited[i]) {
-                continue;
-            }
-            str.append(i);
-            visited[i] = true;
-            helper(list, str, index+1,visited, n);
-            visited[i] = false;
-            str.setLength(len);
+            fact[i] = fact[i - 1] * i;
+            numbers.add(i);
         }
+
+        k--;
+        StringBuilder result = new StringBuilder();
+
+        for (int i = n; i > 0; i--) {
+            int index = k / fact[i - 1];
+            result.append(numbers.get(index));
+            numbers.remove(index);
+
+            k = k % fact[i - 1];
+        }
+
+        return result.toString();
     }
 }
