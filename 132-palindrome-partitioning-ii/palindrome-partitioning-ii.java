e44602dbde55ba162741dee539c79d9336b43e4a
Pranav Deshmukh <151390018+prn9v@users.noreply.github.com>
2026-03-20 19:24:08 +0530

    Time: 30 ms (75.41%) | Memory: 47 MB (67.10%) - LeetSync

---
+++ 132-palindrome-partitioning-ii/palindrome-partitioning-ii.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int minCut(String s) {
+        int n = s.length();
+
+        boolean[][] isPal = new boolean[n][n];
+
+        // Precompute palindrome
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    if (j - i <= 2 || isPal[i + 1][j - 1]) {
+                        isPal[i][j] = true;
+                    }
+                }
+            }
+        }
+
+        int[] dp = new int[n];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+
+        for (int i = 0; i < n; i++) {
+            if (isPal[0][i]) {
+                dp[i] = 0;
+                continue;
+            }
+
+            for (int j = 1; j <= i; j++) {
+                if (isPal[j][i]) {
+                    dp[i] = Math.min(dp[i], dp[j - 1] + 1);
+                }
+            }
+        }
+
+        return dp[n - 1];
+    }
+}
