b1876106a79ec35533d40c6b59ce703776bfb010
Pranav Deshmukh <151390018+prn9v@users.noreply.github.com>
2026-06-15 16:43:47 +0530

    Time: 107 ms (38.52%) | Memory: 47.4 MB (6.18%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,34 +1,29 @@
 class Solution {
     public int findTargetSumWays(int[] nums, int target) {
+        Map<String,Integer> dp = new HashMap<>();
+        return helper(dp,nums,target,0,0);
+    }
 
-        int totalSum = 0;
-        int zeroCount = 0;
-
-        for (int num : nums) {
-            totalSum += num;
-            if (num == 0) zeroCount++;
-        }
-
-        if (totalSum < Math.abs(target) || (target + totalSum) % 2 != 0) {
+    private int helper(Map<String,Integer> dp, int[] nums, int target,int sum,int index) {
+        if(index == nums.length) {
+            if(sum == target) {
+                return 1;
+            }
             return 0;
         }
 
-        int p = (target + totalSum) / 2;
-
-        int[][] dp = new int[nums.length + 1][p + 1];
-
-        dp[0][0] = 1;   // only this
-
-        for (int i = 1; i <= nums.length; i++) {
-            for (int j = 0; j <= p; j++) {
-                dp[i][j] = dp[i - 1][j];
-
-                if (j >= nums[i - 1]) {
-                    dp[i][j] += dp[i - 1][j - nums[i - 1]];
-                }
-            }
+        String key = sum + " " + index;
+        
+        if(dp.containsKey(key)){
+            return dp.get(key);
         }
 
-        return dp[nums.length][p];
+        int withMinus = helper(dp, nums, target, sum - nums[index], index + 1);
+        int withPlus = helper(dp, nums, target, sum + nums[index], index + 1);
+
+        int ans = withMinus + withPlus;
+
+        dp.put(key,ans);
+        return ans;
     }
 }
