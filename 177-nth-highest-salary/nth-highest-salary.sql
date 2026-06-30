a5dd03caf7a738355c4821a54a92246027a1d679
Pranav Deshmukh <151390018+prn9v@users.noreply.github.com>
2026-06-30 17:04:45 +0530

    Time: 539 ms (40.33%) | Memory: 0B (100.00%) - LeetSync

--- 177-nth-highest-salary/nth-highest-salary.sql
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -1,13 +1,13 @@
 CREATE FUNCTION getNthHighestSalary(N IN NUMBER) RETURN NUMBER IS
     result NUMBER;
 BEGIN
-    SELECT MAX(Salary)
+    SELECT MAX(e.Salary)
     INTO result
     FROM (
         SELECT Salary,
                DENSE_RANK() OVER (ORDER BY Salary DESC) ranking
         FROM Employee
-    )
+    ) e
     WHERE ranking = N;
 
     RETURN result;
