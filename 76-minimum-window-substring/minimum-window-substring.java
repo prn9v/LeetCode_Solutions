b7151829aa7142a2b37a94873d8f26564e1e3672
Pranav Deshmukh <151390018+prn9v@users.noreply.github.com>
2026-07-02 17:23:33 +0530

    Time: 16 ms (58.23%) | Memory: 46.9 MB (55.23%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -0,0 +1,51 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        if (s.length() < t.length()) return "";
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char ch : t.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        int dwords = map.size();
+        int i = 0, j = 0;
+
+        int minSize = Integer.MAX_VALUE;
+        int start = 0;
+        while(j < s.length()) {
+            char ch = s.charAt(j);
+
+            if (map.containsKey(ch)) {
+                map.put(ch, map.get(ch) - 1);
+
+                if (map.get(ch) == 0)
+                    dwords--;
+            }
+            
+            while (dwords == 0) {
+
+                if (j - i + 1 < minSize) {
+                    minSize = j - i + 1;
+                    start = i;
+                }
+
+                char left = s.charAt(i);
+
+                if (map.containsKey(left)) {
+                    map.put(left, map.get(left) + 1);
+
+                    if (map.get(left) > 0)
+                        dwords++;
+                }
+
+                i++;
+            }   
+            j++;
+        }
+
+        return minSize == Integer.MAX_VALUE
+                ? ""
+                : s.substring(start, start + minSize);
+    }
+}
