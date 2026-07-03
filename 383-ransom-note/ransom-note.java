4f6535b438a496251519abbc085c974a5130d13e
Pranav Deshmukh <151390018+prn9v@users.noreply.github.com>
2026-07-03 15:27:26 +0530

    Time: 1 ms (99.97%) | Memory: 46.2 MB (79.08%) - LeetSync

---
+++ 383-ransom-note/ransom-note.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] freq = new int[26];
+
+        for(char ch : ransomNote.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+
+        for(char ch : magazine.toCharArray()) {
+            if(freq[ch-'a'] > 0) {
+                freq[ch-'a']--;
+            }
+        }
+
+        for(int i = 0; i < freq.length; i++) {
+            if(freq[i] > 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
