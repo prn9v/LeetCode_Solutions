18e2731aeab6258a80f89dcd4c56a30d7ddd3716
Pranav Deshmukh <151390018+prn9v@users.noreply.github.com>
2026-07-06 23:16:29 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (51.67%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -19,38 +19,32 @@ class Solution {
 
         Node curr = head;
 
-        // 1. Insert copied nodes after each original node
-        while (curr != null) {
+        while(curr != null) {
             Node copy = new Node(curr.val);
             copy.next = curr.next;
             curr.next = copy;
             curr = copy.next;
         }
 
-        // 2. Assign random pointers to copied nodes
         curr = head;
-        while (curr != null) {
-            if (curr.random != null) {
+
+        while(curr != null) {
+            if(curr.random != null) {
                 curr.next.random = curr.random.next;
             }
             curr = curr.next.next;
         }
 
-        // 3. Separate the copied list and restore the original list
         Node dummy = new Node(0);
         Node copyCurr = dummy;
         curr = head;
 
-        while (curr != null) {
-            Node copy = curr.next;
-
-            copyCurr.next = copy;
-            copyCurr = copy;
-
-            curr.next = copy.next;   
+        while(curr != null) {
+            copyCurr.next = curr.next;
+            copyCurr = copyCurr.next;
+            curr.next = curr.next.next;
             curr = curr.next;
         }
-
         return dummy.next;
     }
 }
