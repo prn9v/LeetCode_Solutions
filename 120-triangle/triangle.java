3b95b2f7dd536dab48db615590b7c1a81bf5be95
Pranav Deshmukh <151390018+prn9v@users.noreply.github.com>
2026-07-15 19:44:13 +0530

    Time: 2 ms (84.55%) | Memory: 46.8 MB (7.90%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,20 +1,30 @@
 class Solution {
+
     public int minimumTotal(List<List<Integer>> triangle) {
-        int n = triangle.size();
-        Integer[][] dp = new Integer[n][n];
-        return helper(dp, 0, 0, triangle);
+        Integer[][] dp = new Integer[triangle.size()+1][triangle.size()+1];
+        return helper(0,0,triangle,dp);
     }
 
-    private int helper(Integer[][] dp, int row, int col, List<List<Integer>> triangle) {
-        if(row == triangle.size()) {
+    private int helper(int row, int col, List<List<Integer>> list, Integer[][] dp) {
+        if(row >= list.size() || col >= list.get(row).size()) {
             return 0;
         }
 
-        if (dp[row][col] != null) return dp[row][col];
+        if(row == list.size() - 1) {
+            return list.get(row).get(col);
+        }
 
-        int down = helper(dp, row + 1, col, triangle);
-        int diag = helper(dp, row + 1, col + 1, triangle);
+        if(dp[row][col] != null) {
+            return dp[row][col];
+        }
 
-        return dp[row][col] = triangle.get(row).get(col) + Math.min(down, diag);
+        int ans = Integer.MAX_VALUE;
+
+        int sum1 = list.get(row).get(col) + helper(row+1,col,list,dp);
+        int sum2 = list.get(row).get(col) + helper(row+1,col+1,list,dp);
+
+        ans = Math.min(sum1,sum2);
+
+        return dp[row][col] = ans;
     }
 }
