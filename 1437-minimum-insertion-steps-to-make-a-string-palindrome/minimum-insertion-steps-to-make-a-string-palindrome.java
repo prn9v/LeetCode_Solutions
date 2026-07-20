9391facd8df57b28f8c86a1ac324594d63e4cdac
Pranav Deshmukh <151390018+prn9v@users.noreply.github.com>
2026-07-20 23:26:06 +0530

    Time: 30 ms (25.16%) | Memory: 47.5 MB (39.49%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,30 +1,26 @@
 class Solution {
     public int minInsertions(String s) {
         String rev = new StringBuilder(s).reverse().toString();
-        int lps = lcs(s,rev,s.length());
-
-        return s.length() - lps;
+        return s.length() - lcs(s,rev);
     }
 
-    private int lcs(String s,String rev,int len){
-        int[][] dp = new int[len+1][len+1];
-
-        Arrays.fill(dp[0],0);
-
-        for(int i = 0; i <= len; i++){
-            dp[i][0] = 0;
+    private int lcs(String s1, String s2) {
+        if(s1.length() == 0 || s2.length() == 0) {
+            return 0;
         }
-
-        for(int i = 1; i < dp.length; i++){
-            for(int j = 1; j < dp[0].length; j++){
-                if(s.charAt(i-1) == rev.charAt(j-1)){
-                    dp[i][j] = 1 + dp[i-1][j-1]; 
+        
+        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+        
+        for(int i = 1; i <= s1.length(); i++) {
+            for(int j = 1; j <= s2.length(); j++) {
+                if(s1.charAt(i-1) == s2.charAt(j-1)) {
+                    dp[i][j] = 1 + dp[i-1][j-1];
                 } else {
                     dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
-
-        return dp[len][len];
+        
+        return dp[s1.length()][s2.length()];
     }
 }
