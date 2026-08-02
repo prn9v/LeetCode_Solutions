6cac7770afe15495bf91532e22d266fef78693aa
Pranav Deshmukh <151390018+prn9v@users.noreply.github.com>
2026-08-02 15:06:56 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (62.03%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,10 +1,9 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        HashMap<String,Boolean> map = new HashMap<>();
-        return helper(map,piles,0,piles.length-1,0,0,true);
+        return helper(piles,0,piles.length-1,0,0,true);
     }
 
-    private boolean helper(HashMap<String,Boolean> map, int[] piles, int i, int j, int sum1, int sum2, boolean isPlayerOne) {
+    private boolean helper(int[] piles, int i, int j, int sum1, int sum2, boolean isPlayerOne) {
         if(i > j) {
             if(sum1 > sum2) {
                 return true;
@@ -13,30 +12,17 @@ class Solution {
             return false;
         }
 
-        String key = i + "_" + j + "_" + sum1 + "_" + sum2 + "_" + isPlayerOne;
-        if(map.containsKey(key)){
-            return map.get(key);
-        }
-
         if(isPlayerOne){
             if(piles[i] >= piles[j]) {
-                boolean val = helper(map,piles,i+1,j,sum1+piles[i],sum2,false);
-                map.put(key,val);
-                return val;
+                return helper(piles,i+1,j,sum1+piles[i],sum2,false);
             } else {
-                boolean val = helper(map,piles,i,j-1,sum1+piles[j],sum2,false);
-                map.put(key,val);
-                return val;
+                return helper(piles,i,j-1,sum1+piles[j],sum2,false);
             }
         } else {
             if(piles[i] >= piles[j]) {
-                boolean val = helper(map,piles,i,j-1,sum1,sum2+piles[j],true);
-                map.put(key,val);
-                return val;
+                return helper(piles,i,j-1,sum1,sum2+piles[j],true);
             } else {
-                boolean val = helper(map,piles,i+1,j,sum1,sum2+piles[i],true);
-                map.put(key,val);
-                return val;
+                return helper(piles,i+1,j,sum1,sum2+piles[i],true);
             }
         }
     }
