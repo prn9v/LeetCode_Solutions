bd304513f51e0f54d9536d29cb5ab3100ffbc58a
Pranav Deshmukh <151390018+prn9v@users.noreply.github.com>
2026-08-02 15:06:20 +0530

    Time: 14 ms (5.73%) | Memory: 49.9 MB (5.73%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,9 +1,10 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        return helper(piles,0,piles.length-1,0,0,true);
+        HashMap<String,Boolean> map = new HashMap<>();
+        return helper(map,piles,0,piles.length-1,0,0,true);
     }
 
-    private boolean helper(int[] piles, int i, int j, int sum1, int sum2, boolean isPlayerOne) {
+    private boolean helper(HashMap<String,Boolean> map, int[] piles, int i, int j, int sum1, int sum2, boolean isPlayerOne) {
         if(i > j) {
             if(sum1 > sum2) {
                 return true;
@@ -12,17 +13,30 @@ class Solution {
             return false;
         }
 
+        String key = i + "_" + j + "_" + sum1 + "_" + sum2 + "_" + isPlayerOne;
+        if(map.containsKey(key)){
+            return map.get(key);
+        }
+
         if(isPlayerOne){
             if(piles[i] >= piles[j]) {
-                return helper(piles,i+1,j,sum1+piles[i],sum2,false);
+                boolean val = helper(map,piles,i+1,j,sum1+piles[i],sum2,false);
+                map.put(key,val);
+                return val;
             } else {
-                return helper(piles,i,j-1,sum1+piles[j],sum2,false);
+                boolean val = helper(map,piles,i,j-1,sum1+piles[j],sum2,false);
+                map.put(key,val);
+                return val;
             }
         } else {
             if(piles[i] >= piles[j]) {
-                return helper(piles,i,j-1,sum1,sum2+piles[j],true);
+                boolean val = helper(map,piles,i,j-1,sum1,sum2+piles[j],true);
+                map.put(key,val);
+                return val;
             } else {
-                return helper(piles,i+1,j,sum1,sum2+piles[i],true);
+                boolean val = helper(map,piles,i+1,j,sum1,sum2+piles[i],true);
+                map.put(key,val);
+                return val;
             }
         }
     }
